00036366b47246b16fd14eb3e5f814266f342d75
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-29 13:26:01 +0330

    added pages components scaffolding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/DefaultPage";
 
 function App() {
-  return (
-    <h1>app</h1>
-  );
+  return <h1>app</h1>;
 }
 
 export default App;
---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductsPage() {
+  return (
+    <>
+      <h1>product</h1>
+    </>
+  );
+}
