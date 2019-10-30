8727e56833956d4d1021607b43b74d7dd000a948
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-30 11:23:29 +0330

    added Navbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,6 @@ export default function HomePage() {
       <>
         <ProductConsumer>
           {value => {
-            console.log(value);
             return <h1>home</h1>
           }}
         </ProductConsumer>      
