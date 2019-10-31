bf620e0993e4c70ed99d70e0d8e23af9c000fe2d
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-31 20:25:31 +0330

    added Services component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { ProductConsumer } from '../context'
 import Hero from '../components/Hero'
 import { Link } from 'react-router-dom'
+import Services from '../components/HomePage/Services'
+import Featured from '../components/HomePage/Featured'
 
 export default function HomePage() {
 
@@ -10,6 +11,8 @@ export default function HomePage() {
         <Hero title="awesome gadgets" max="true">
           <Link className="main-link" style={{margin:"2rem"}} to="/products">our products</Link>
         </Hero>
+        <Services/>
+        <Featured/> 
       </>
   );
 }
