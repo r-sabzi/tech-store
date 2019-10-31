1dbd774ca8a9179d40ad62026dd4015f9a708d9d
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-31 21:29:23 +0330

    added temp offline data fetching functions

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { linkData } from './linkData'
 import { socialData } from './socialData'
+import { items } from './productData'
 
 const ProductContext = React.createContext();
 
@@ -9,12 +10,62 @@ class ProductProvider extends Component{
   state = {
     sidebarOpen: false,
     cartOpen: false,
-    cartItems: 0,
     links: linkData,
     socialIcons: socialData,
-    cart:[]
+    cart: [],
+    cartItems: 0,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+    singleProduct:{},
+    loading:false
   }
 
+  componentDidMount() {
+    this.setProducts(items);
+  }
+  
+  setProducts = (products) => {
+    let storeProducts = products.map(item => {
+      const { id } = item.sys;
+      const image = item.fields.image.fields.file.url;
+      const product = { id, ...item.fields,image };
+      return product
+    }
+    );
+    let featuredProducts = storeProducts.filter(item => item.featured === true);
+    
+    this.setState({
+      storeProducts,
+      filteredProducts: storeProducts,
+      featuredProducts,
+      cart: this.getStorageCart(),
+      singleProduct: this.getStorageProduct(),
+      loading:false
+    })
+  }
+  getStorageCart = () => {
+    return [];
+  }
+  getStorageProduct = () => {
+    return [];
+  }
+  getTotal = () => { };
+
+  addTotal = () => { };
+
+  syncStorage = () => { };
+
+  addToCart = (id) => {
+    console.log(`add to cart${id}`);
+  }
+  setSingleProduct = (id) => {
+    console.log(`single product${id}`);
+  }
+  
   handleSidebar=() => {
     this.setState({sidebarOpen:!this.state.sidebarOpen})
   }
@@ -35,7 +86,9 @@ class ProductProvider extends Component{
         handleSidebar :this.handleSidebar,
         handleCart :this.handleCart,
         openCart :this.openCart,
-        closeCart :this.closeCart
+        closeCart: this.closeCart,
+        addToCart: this.addToCart,
+        setSingleProduct:this.setSingleProduct
     }}>
       {this.props.children}
     </ProductContext.Provider>
