d594ca1145cfedc8d2d2d9e07db743f109a921d7
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-02 17:44:20 +0330

    added layout for Featured component

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Product from '../Product'
+import Title from '../Title'
+import { Link } from 'react-router-dom'
+import { productConsumer, ProductConsumer } from '../../context'
+
 
 export default function Featured() {
   return (
-    <div>
-      featured
-    </div>
+    <section className="py-5">
+      <div className="container">
+        <Title title="featured products" center="true" />
+        <div className="row">
+          <ProductConsumer>
+            {value => {
+              const { featuredProducts} = value;
+              return featuredProducts.map( product => {
+                  return <Product key={Product.id} product={product}/>
+              })
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
   )
-}
+} 
