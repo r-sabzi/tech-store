33635a79c3cfedd6c1b2970b03ed5bcd12ae170c
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-05 17:38:54 +0330

    finished SideCart component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -7,9 +7,27 @@ export default function SideCart() {
 
   return <ProductConsumer>
     {value => {
-      const {cartOpen,closeCart,cart}=value;
+      const {cartOpen,closeCart,cart,cartTotal}=value;
       return <CartWrapper show={cartOpen} onClick={closeCart} >
-        hello cart
+        <ul>
+          {cart.map(item => {
+            return (
+              <li key={item.id} className="cart-item mb-4">
+                <img width="35" src={`../${item.image}`} alt="cart item" />
+                <div className="mt-3">
+                  <h6 className="text-uppercase">{item.title}</h6>
+                  <h6 className="text-title text-capitalize">amount : {item.count}</h6>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+        <h4 className="text-capitalize text-main">cart total : {cartTotal}</h4>
+        <div className="text-center my-5">
+          <Link to="/cart" className="main-link" >
+            cart page
+          </Link>
+        </div>
       </CartWrapper>
     }}
   </ProductConsumer>
@@ -28,5 +46,13 @@ const CartWrapper = styled.div`
   
   @media(min-width:576px){
     width:20rem;
+  }
+  overflow:scroll;
+  padding: 2rem;
+  ul{
+    padding:0 !important
+  }
+  .cart-item{
+    list-style-type:none;    
   }
 `
